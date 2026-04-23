adb9b32e139f4a9fa07d554c79c40bde878bf978
Namita Shreyasee <shreyaseenamita2k06@gmail.com>
2026-04-23 12:39:25 +0530

    Added basic linked list operations

---
+++ LinkedList/LL Basics/Main.java
@@ -0,0 +1,33 @@
+public class Main{
+    public static void main(String[] args) {
+//        sll list = new sll();
+//         list.insertFirst(3);
+//          list.insertFirst(4);
+//           list.insertFirst(8);
+//            list.insertFirst(12);
+//            list.insertLast(99);
+//            list.insert(100,3);
+//             list.display();
+//           System.out.println("  "+ list.deleteFirst());
+//           list.display();
+//           System.out.println("  "+ list.deleteLast());
+//           list.display();
+//            System.out.println(list.delete(2));
+//            list.display();
+//  System.out.println(list.find (8));
+//  list.display();
+dll list = new dll();
+ list.insertFirst(3);
+         list.insertFirst(2);
+          list.insertFirst(8);
+           list.insertFirst(17);
+           list.displayRev();
+           list.insertLast(100);
+           
+           list.insert(3, 99);
+           list.display();
+
+
+
+    }
+}
